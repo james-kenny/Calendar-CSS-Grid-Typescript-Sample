27cd912c235040d9aa16f3cf6a9421f3084f6ad2
James Kenny <james@extractorapp.com>
2018-07-03 10:31:04 +0200

    added onclick function

--- cssgridcalendar/content/js/site.js
+++ cssgridcalendar/content/js/site.js
@@ -1,4 +1,10 @@
+// Just the starting part of the typescript side. For now I'm working on the CSS Grid part this will come later.
 var dateclicked = function (item) {
-    console.log('A date has been clicked!');
-    console.log('We should do something more here at some point.');
+    console.log('A date has been clicked! - ' + item);
 };
+function findtoday() {
+    var Wday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var day = new Date();
+    var TodayDay = Wday[day.getDay()];
+    document.getElementById("today-date").innerText = day.toDateString();
+}
