2be4db2f1138008b67e806d5ef33b898e788ee46
James Kenny <james@extractorapp.com>
2018-07-03 09:49:36 +0200

    added comment on TS file

--- cssgridcalendar/content/js/site.ts
+++ cssgridcalendar/content/js/site.ts
@@ -1,3 +1,5 @@
+// Just the starting part of the typescript side. For now I'm working on the CSS Grid part this will come later.
+
 var dateclicked = function (item) {
     console.log('A date has been clicked!');
     console.log('We should do something more here at some point.');
